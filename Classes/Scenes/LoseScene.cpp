0dc0ea19cbcdf83b3c0d63b842eea960a2a323eb
Kim Ninh Huynh <kimninh1610@gmail.com>
2019-06-09 22:26:54 +0700

    update Lose Game Scene

---
+++ Classes/Scenes/LoseScene.cpp
@@ -0,0 +1,71 @@
+#include "LoseScene.h"
+#include "StartMenuScene.h"
+
+ui::Button* TetrisGame::LoseScene::btnRestart = nullptr;
+ui::Button* TetrisGame::LoseScene::btnHighScore = nullptr;
+ui::Button* TetrisGame::LoseScene::btnMainMenu = nullptr;
+ui::Button* TetrisGame::LoseScene::btnQuit = nullptr;
+Label* TetrisGame::LoseScene::txtScore = nullptr;
+Label* TetrisGame::LoseScene::txtTime = nullptr;
+
+void TetrisGame::LoseScene::handleButtonsClick()
+{
+	auto menuClick_SFX_Path = "sfx/click5.ogg";
+
+	btnRestart->addClickEventListener([=](Ref*)
+	{
+		experimental::AudioEngine::play2d(menuClick_SFX_Path);
+	});
+
+	btnHighScore->addClickEventListener([=](Ref*)
+	{
+		experimental::AudioEngine::play2d(menuClick_SFX_Path);
+	});
+
+	btnMainMenu->addClickEventListener([=](Ref*)
+	{
+		experimental::AudioEngine::play2d(menuClick_SFX_Path);
+		auto scene = StartMenuScene::createScene();
+
+		Director::getInstance()->popToRootScene();
+		Director::getInstance()->replaceScene(scene);
+	});
+
+	btnQuit->addClickEventListener([=](Ref*)
+	{
+		experimental::AudioEngine::play2d(menuClick_SFX_Path);
+		Director::getInstance()->end();
+	});
+}
+
+cocos2d::Scene* TetrisGame::LoseScene::createScene()
+{
+	creator::CreatorReader* reader = creator::CreatorReader::createWithFilename("creator/scenes/LoseMenu.ccreator");
+	reader->setup();
+	Scene* scene = reader->getSceneGraph();
+
+	btnRestart = dynamic_cast<ui::Button*>(scene->getChildByName("btnRestart"));
+	btnHighScore = dynamic_cast<ui::Button*>(scene->getChildByName("btnHighScore"));
+	btnMainMenu = dynamic_cast<ui::Button*>(scene->getChildByName("btnMainMenu"));
+	btnQuit = dynamic_cast<ui::Button*>(scene->getChildByName("btnQuit"));
+	txtScore = dynamic_cast<Label*>(scene->getChildByName("txtScore"));
+	txtTime = dynamic_cast<Label*>(scene->getChildByName("txtTime"));
+	auto *galaxy_bg = dynamic_cast<Sprite*>(scene->getChildByName("galaxy-bg"));
+	
+	txtScore->setString("1000");
+	txtTime->setString("01:59");
+
+	// add background scoller to this scene
+	BackgroundScroller bg_scroller(scene, galaxy_bg, 100, 1330, 0);
+	bg_scroller.startScroll();
+
+	// click event
+	handleButtonsClick();
+
+	return scene;
+}
+
+bool TetrisGame::LoseScene::init()
+{
+	return true;
+}
--- Classes/Scenes/LoseScene.h
+++ Classes/Scenes/LoseScene.h
@@ -1,9 +1,30 @@
 #ifndef LOSESCENE_H
 #define LOSESCENE_H
+
 #include "cocos2d.h"
+#include "CreatorReader.h"
+#include "BackgroundScroller.h"
+#include "AudioEngine.h"
+
+USING_NS_CC;
 
 namespace TetrisGame {
 	class LoseScene : cocos2d::Scene {
+	private:
+
+		static ui::Button* btnRestart;
+		static ui::Button* btnHighScore;
+		static ui::Button* btnMainMenu;
+		static ui::Button* btnQuit;
+		static Label* txtScore;
+		static Label* txtTime;
+		static void handleButtonsClick();
+
+	public:
+		static cocos2d::Scene* createScene();
+		bool init() override;
+
+		CREATE_FUNC(LoseScene);
 	};
 }
 
